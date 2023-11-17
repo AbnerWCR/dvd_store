565176b32aeb19a4ba6a3f5ddeebbaf8adc3180c
abner_wallace <abnerwcrodrigues@gmail.com>
2023-11-16 23:06:44 -0300

    test: TDD refactor stg_actor

--- models/stg_actor.py
+++ models/stg_actor.py
@@ -1,6 +1,13 @@
+from sqlalchemy import Column, Integer, DateTime, Boolean, String
+from sqlalchemy.ext.declarative import declarative_base
 
-class StgActor():
-    def __init__(self, bk_actor, first_name, last_name):
-        self.bk_actor = bk_actor
-        self.first_name = first_name
-        self.last_name = last_name
+Base = declarative_base()
+
+
+class StgActor(Base):
+    __tablename__ = "s_actor"
+    __table_args__ = {"schema": "stg"}
+
+    bk_actor = Column("bk_actor", Integer, primary_key=True)
+    first_name = Column("first_name", String(50))
+    last_name = Column("last_name", String(50))
--- tests/test_model_stg_actor.py
+++ tests/test_model_stg_actor.py
@@ -3,7 +3,7 @@ from models.stg_actor import StgActor
 
 def test_stg_actor_create():
     try:
-        stg_actor = StgActor(bk_actor="1",
+        stg_actor = StgActor(bk_actor=1,
                              first_name="abner",
                              last_name="wallace")
         assert stg_actor is not None
