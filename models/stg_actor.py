ccaa5aabef91c6c6c294b4e3c4c9a414ce6b52fe
abner_wallace <abnerwcrodrigues@gmail.com>
2023-11-16 22:45:44 -0300

    feat: implement class StgActor TDD green

---
+++ models/stg_actor.py
@@ -0,0 +1,6 @@
+
+class StgActor():
+    def __init__(self, bk_actor, first_name, last_name):
+        self.bk_actor = bk_actor
+        self.first_name = first_name
+        self.last_name = last_name
