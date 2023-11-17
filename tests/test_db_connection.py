73d5330e108e438065ab5ba1bd706cdd309633fb
abner_wallace <abnerwcrodrigues@gmail.com>
2023-11-16 22:04:35 -0300

    test: TDD green infra

---
+++ tests/test_db_connection.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import configparser
+from urllib.parse import quote
+
+
+def test_dw_connection():
+    try:
+        config = configparser.ConfigParser()
+        config.read("pyconf.ini")
+        server = config['DATABASE']['server']
+        port = config['DATABASE']['port']
+        username = config['DATABASE']['username']
+        password = config['DATABASE']['password']
+        database = config['DATABASE']['database']
+        connection_string = f"postgresql://{username}:{password}@{server}:{port}/{database}"
+        engine: create_engine = create_engine(connection_string)
+        dw_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        assert True
+    except Exception as ex:
+        assert False
